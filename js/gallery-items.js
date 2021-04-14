f9756ae0b9d353d89475d271c85b1c4ff2457519
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-04-14 03:55:19 +0300

    Добавляет немного новых картинок

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -62,4 +62,67 @@ export default [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/10/24/19/58/babys-hand-3771123__340.jpg',
+    original:
+      'https://pixabay.com/get/g39a93709b0ab11bbd5e3353f51525af8488329d6656de592b1ff3a519befae31ef72a99c76e2ad446b38c7c409213678335fc44978359885af133208b1e08e69_1280.jpg',
+    description: "Child's Hand",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/04/21/07/18/easter-4143537__340.jpg',
+    original:
+      'https://pixabay.com/get/g60a02462aa0c39f05f31ea4275ae5ee381a55d6e1ec8f90212f96b8ed76e33c256737d1de070138fb33ea217328014fc93c683f656af4b79927575327dacb1dd_1280.jpg',
+    description: "Child on the hay with easter basket",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/05/16/23/leopard-6153930__340.jpg',
+    original:
+      'https://pixabay.com/get/gd1e45a4bf5f92759bf4a8fe0dba23d37902bde9c489b564f3ec5a4b4aa1ef151832d8ca43951893ffe9af56adaead58ecaa23043ce5525a675e3bc82d27dc535_1280.jpg',
+    description: "Amur leopard",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/12/21/08/36/dog-5849152__340.jpg',
+    original:
+      'https://pixabay.com/get/ge2386b3d4f8414ca8ab9b4b35d48a21509cdf950224f0400dabd5a09fa6db14c7064d10c530ea0efc87a6caf1aef0a0168bda76c147eda5e1b61968190f86e09_1280.jpg',
+    description: "Cute puppy",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/03/17/13/04/desert-4940300__340.jpg',
+    original:
+      'https://pixabay.com/get/g864885082fd481b53ee361a1c077f4938f9433c60475765ed253b02643ebab2cc4b9b752622b58b800f58f80f944264371bc976805f97663f345e7f4bdd97e34_1280.jpg',
+    description: "Desert, Wadi Rum, Jordan, Sand Stone",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/01/03/11/44/woman-4737919__340.jpg',
+    original:
+      'https://pixabay.com/get/g8c8a2a2a26d66279affb70bb3eb722a6b83eb5d55a5f8e789a169a9b1ff85d3e6fca1580cb9eff112bc9faf33f904d7ed4fefa3b53c920f2727c826c3dfbd443_1280.jpg',
+    description: "Woman on a rocky shore",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/02/09/18/55/bastei-4834233__340.jpg',
+    original:
+      'https://pixabay.com/get/g784e66e4e4654208b74d42bbde614fb53d38f5ed7e67fccc942e94b1a11dd3eff395052950cd5e235c5138f67d2168e18bae17a1c776afe3aec367c02bfc3dff_1280.jpg',
+    description: "Bastei",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/01/22/18/23/bridge-4785964__340.jpg',
+    original:
+      'https://pixabay.com/get/gccb1fc63b5b60da0ff2558769c2a3278bfa8cb6824450b7a6f24784b3c07f180ee09432f7a0001cfc7aa5d3aba18ae7c8c71f0f3baeef0771acea34ca016f5ea_1280.jpg',
+    description: "Bridge on the background of the night city",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/11/02/57/mountain-6086083__340.jpg',
+    original:
+      'https://pixabay.com/get/g0cf9312f75bd79a01a44c16ee09465c21706c6ea030ca04f8a02c856f635e3ff7183f7b2fe37d9fc7ddcd6fc33c45b4664c206bc4d0ed636d4dbb9bc49a9cfcd_1280.jpg',
+    description: "Sunny dawn over a snowy mountain",
+  },
 ];
